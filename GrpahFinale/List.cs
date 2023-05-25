823dd6f404f5353e0a9b20e3a2bcf3633013aabb
RybalovMS <RybalovMS@402D-12>
2023-05-25 15:41:09 +0700

    Добавьте файлы проекта.

---
+++ GrpahFinale/List.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GrpahFinale
+{
+    class MyList<T>
+    {
+        public Node<T> first;
+        public Node<T> last;
+
+        public MyList()
+        {
+            first = null;
+            last = null;
+        }
+    }
+}
---
+++ GrpahFinale/Stack.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GrpahFinale
+{
+    class MyStack<T>
+    {
+        public MyList<T> list;
+
+        public MyStack()
+        {
+            list = new MyList<T>();
+        }
+
+        public void Push(T data)
+        {
+            Node<T> node1 = new Node<T>(data);
+            node1.next = list.first;
+            list.first = node1;
+        }
+    }
+}
